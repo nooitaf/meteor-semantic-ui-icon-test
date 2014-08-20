22764a1a310c03df99caea7b8c65b9764530248d
nooitaf <nooitaf@gmail.com>
2014-08-20 15:09:52 +0200

    added files

---
+++ semantic-ui-0.19.0-test.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to semantic-ui-0.19.0-test.";
+  };
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
